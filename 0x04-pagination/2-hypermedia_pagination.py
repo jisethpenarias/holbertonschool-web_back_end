d577eb550e251d95a53b23183f463f2ae3129e14
jisethpenarias <1875@holbertonschool.com>
2021-11-01 19:13:41 -0500

    task 2

---
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+"""
+Hypermedia pagination
+"""
+
+import csv
+import math
+from typing import Tuple, List, Dict
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        method named get_page that takes two integer arguments page
+        with default value 1 and page_size with default value 10.
+        """
+
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+        gp_range = index_range(page, page_size)
+        gp_start = gp_range[0]
+        gp_end = gp_range[1]
+        return self.dataset()[gp_start:gp_end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """
+        method that takes the same arguments (and defaults) as get_page
+        and returns a dictionary containing the following key-value pairs:
+        """
+
+        page_data = self.get_page(page, page_size)
+        total_data = len(self.dataset())
+        total_pages = math.ceil(total_data / page_size)
+        dictionary_info = {
+            'page_size': len(page_data),
+            'page': page,
+            'data': page_data,
+            'next_page': page + 1 if page < total_pages else None,
+            'prev_page': page - 1 if page != 1 else None,
+            'total_pages': total_pages
+        }
+
+        return dictionary_info
