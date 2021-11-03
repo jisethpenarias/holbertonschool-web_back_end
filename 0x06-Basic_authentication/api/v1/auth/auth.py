a2ee410edcb78001e7aa1a9a7cd02d4a1864a2c8
jisethpenarias <1875@holbertonschool.com>
2021-11-02 23:29:23 -0500

    3. Auth class

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+""" Class auth """
+
+
+from flask import request
+
+
+class Auth:
+    """ Class Auth """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ public method require auth """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ public method authorization header """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ public method current_user """
+        return None
