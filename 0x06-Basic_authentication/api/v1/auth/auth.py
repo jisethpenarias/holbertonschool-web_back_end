0a6b46fbe76e61c6db2eff7be59c2422cfaeaa8c
jisethpenarias <1875@holbertonschool.com>
2021-11-03 23:02:57 -0500

    fix

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -12,20 +12,14 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ public method require auth """
-        if path and excluded_paths:
-            if path[-1] != '/':
-                path += '/'
-            for pth in excluded_paths:
-
-                path = path.replace('/', '')
-                pth = pth.replace('/', '')
-
-                if pth[-1] == '*':
-                    pth = pth.replace('*', '.*')
-
-                if re.search(pth, path):
-                    return False
-        return True
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """ public method authorization header """
