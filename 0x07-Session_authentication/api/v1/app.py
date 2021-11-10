00411c1b97e40102dec6bc964cf16bc26dfd4b11
jisethpenarias <1875@holbertonschool.com>
2021-11-09 22:57:20 -0500

    Before request

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -35,9 +35,11 @@ def request_filter() -> None:
     if auth is not None:
         if not auth.require_auth(request.path, ['/api/v1/status/',
                                                 '/api/v1/unauthorized/',
-                                                '/api/v1/forbidden/']):
+                                                '/api/v1/forbidden/',
+                                                'api/v1/auth_session/login/']):
             return
-        if auth.authorization_header(request) is None:
+        if auth.authorization_header(request) is None \
+                and auth.session_cookie(request) is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
